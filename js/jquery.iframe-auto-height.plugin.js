306cf49ed179a21281436d0ab9d6283ecc4c4a08
ChristineP2 <christine.panus@panusventures.com>
2011-08-16 17:11:50 -0500

    Modified to allow for an optional minimum height in the options struct.

--- js/jquery.iframe-auto-height.plugin.js
+++ js/jquery.iframe-auto-height.plugin.js
@@ -22,6 +22,7 @@
     // set default option values
     var options = $.extend({
         heightOffset: 0, 
+        minHeight: null, 
         callback: function (newHeight) {},
         debug: false
       }, spec);
@@ -61,6 +62,10 @@
         } else {
           newHeight = iframe.contentWindow.document.body.offsetHeight + options.heightOffset;
         }
+        if ( parseInt(options.minHeight) != 0 && newHeight < options.minHeight)
+  				{
+						newHeight = options.minHeight;
+					}
         debug("New Height: " + newHeight);
         iframe.style.height = newHeight + 'px';
         options.callback({newFrameHeight: newHeight});
